10023e18d456ec0ad2a337a295e288fab02ffed3
maccali <guilherme.maccali@universo.univates.br>
2020-02-13 10:31:48 -0300

    Limpeza de plugins, algumas coisas mais

--- wp-content/themes/portifolio/components/contato.php
+++ wp-content/themes/portifolio/components/contato.php
@@ -7,15 +7,67 @@
         </div>
         <div class="row">
             <div class="col-12 col-md-4">
+                <div class="row">
+                    <div class="col-12 titulo-sessao">
+                        <h6><b>Você pode me seguir em alguma delas: </b></h6>
+                    </div>
+                </div>
                 <div class="cont-social-grande">
-                    <a href=""><i class="fab fa-github"></i></a>
-                    <a href=""><i class="fab fa-linkedin"></i></a></br>
-                    <a href=""><i class="fab fa-facebook"></i></a>
-                    <a href=""><i class="fab fa-gmail"></i></a>
+                    <div class="row">
+                        <div class="col-6">
+                            <div class="card-social">
+                                <a href=""><i class="fab fa-github"></i></a>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="card-social">
+                                <a href=""><i class="fab fa-linkedin"></i></a></br>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="card-social">
+                                <a href=""><i class="fab fa-facebook"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                
+            </div>
+            <div class="col-12 col-md-4">
+            <div class="row">
+                    <div class="col-12 titulo-sessao">
+                        <h6><b>Ou mandar um email direto:</b></h6>
+                    </div>
+                </div>
+                
+                <div class="cont-social-grande">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card-social">
+                                <a href=""><i class="fab fa-whatsapp-square"></i></a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="col-12 col-md-8">
-                <?php get_template_part('components/formContato')?>
+            <div class="col-12 col-md-4">
+            <div class="row">
+                    <div class="col-12 titulo-sessao">
+                        <h6><b>Ou mandar um email direto:</b></h6>
+                    </div>
+                </div>
+                
+                <div class="cont-social-grande">
+                    <div class="row">
+                      
+                        <div class="col-12">
+                            <div class="card-social">
+                                <a href=""><i class="fas fa-envelope"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
