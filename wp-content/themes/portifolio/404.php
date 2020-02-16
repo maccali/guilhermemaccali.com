46d186f9f54a1fb44cec1b8ab63dc79a636423ee
maccali <guilherme.maccali@universo.univates.br>
2020-02-15 23:28:54 -0300

    Página 404

--- wp-content/themes/portifolio/404.php
+++ wp-content/themes/portifolio/404.php
@@ -1,8 +1,17 @@
 <?php get_header(); ?>
-    <div class="container-fluid 404">
+    <div class="container-fluid pagina-vazia">
         <div class="container">
-            sdasd
+            <div class="corpo-vazio">
+                <img src="https://picsum.photos/800/800/?blur=1" alt="">
+                <div class="box-zero" data-aos="fade-up">
+                    <div>
+                        <h2>404</h2>
+                        <p>Parece que você se perdeu,</p>
+                        <p>Clique abaixo para voltar onde estava.</p>
+                        <a href="javascript:history.back()" class="btn btn-primary to-croll" >Voltar</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div> 
-
 <?php get_footer();
