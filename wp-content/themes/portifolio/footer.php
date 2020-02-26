f974ccc1cac3146ff82a3b4b687f473c99627c31
maccali <guilherme.maccali@universo.univates.br>
2020-02-26 16:04:35 -0300

    COrreção de https

--- wp-content/themes/portifolio/footer.php
+++ wp-content/themes/portifolio/footer.php
@@ -5,7 +5,7 @@
 <?php get_template_part('components/footer')?>
 
 <!-- JQUARY -->
-<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script type="text/javascript" src="https://code.jquery.com/jquery-latest.min.js"></script>
 
 <!-- BOOTSTRAP -->
 <!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script> -->
