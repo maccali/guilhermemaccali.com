3d1b5b3161314ed0c225e3704b8b3ab4329feec0
maccali <guilherme.maccali@universo.univates.br>
2020-02-26 15:28:45 -0300

    Ajuste na visao do carousel

--- wp-content/themes/portifolio/components/skils.php
+++ wp-content/themes/portifolio/components/skils.php
@@ -1,16 +1,15 @@
 <div class="container-fluid invi">
-    <div class="container">
+    <div class="container" data-aos="fade-up">
         <div class="row">
             <div class="col-12 titulo-sessao ">
-                <h2 data-aos="fade-up">Habilidades</h2>
+                <h2>Habilidades</h2>
             </div>
         </div>
-
         <div class="row">
             <div class="col-12">
                 <?php $galeria = acf_photo_gallery("habilidades", 12)?>
                 <?php $cont = count($galeria, COUNT_NORMAL)?>
-                <div class="skils owl-carousel" data-aos="fade-up">
+                <div class="skils owl-carousel">
                     <?php for ($i = 0; $i < $cont; $i++): ?>
                         <?php $thumbnail_image_url = $galeria[$i]['thumbnail_image_url']?>
                         <?php $title = $galeria[$i]['title']?>
@@ -20,7 +19,5 @@
                 </div>
             </div>
         </div>
-        
-                    
     </div>
 </div> 
