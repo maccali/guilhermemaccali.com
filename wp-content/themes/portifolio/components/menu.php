eae544c97385121c1cce7efc2e9f54036437db5f
maccali <guilherme.maccali@universo.univates.br>
2020-03-29 16:42:18 -0300

    Versionamento

--- wp-content/themes/portifolio/components/menu.php
+++ wp-content/themes/portifolio/components/menu.php
@@ -2,7 +2,7 @@
     <div class="container">
         <nav class="navbar navbar-expand-lg ">
             <a class="navbar-brand"  data-aos="zoom-in" href="<?php echo get_site_url()?>">
-                <img src="<?php echo get_template_directory_uri() . '/img/logo.png' ?>" alt="">
+                <img src="<?php echo get_template_directory_uri() . '/img/logo.png?v3' ?>" alt="">
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <i class="fas fa-bars"></i>
