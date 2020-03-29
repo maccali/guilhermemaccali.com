daca3b132abd8398c47895664053efab194d536d
maccali <guilherme.maccali@universo.univates.br>
2020-03-29 16:30:19 -0300

    Versionamento de arquivo estático

--- wp-content/themes/portifolio/functions.php
+++ wp-content/themes/portifolio/functions.php
@@ -2,7 +2,7 @@
 
 function add_theme_scripts() { 
 
-    wp_enqueue_style( 'style', get_template_directory_uri() . '/css/hp.css', false, '1.4','all');
+    wp_enqueue_style( 'style', get_template_directory_uri() . '/css/hp.css', false, '1.5','all');
     // wp_enqueue_style( 'owl-carousel', get_template_directory_uri() . '/libs/owlCarousel/assets/owl.carousel.min.css', false, '1.1','all');
     // wp_enqueue_style( 'owl-theme', get_template_directory_uri() . '/libs/owlCarousel/assets/owl.theme.default.min.css', false, '1.1','all');
 
